313071634badc6a1b136ec5930b50102363f78f1
Gal Berezansky <gberezansky@lightricks.com>
2020-08-20 14:20:18 +0300

    git testing

---
+++ Matchismo/Model/SetCardDeck.h
@@ -0,0 +1,17 @@
+//
+//  SetCardDeck.h
+//  Matchismo2
+//
+//  Created by Gal Berezansky on 11/08/2020.
+//  Copyright © 2020 Gal Bereznaksy. All rights reserved.
+//
+
+#import "Deck.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SetCardDeck : Deck
+
+@end
+
+NS_ASSUME_NONNULL_END
